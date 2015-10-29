da874019bd4bb0499b2f9305ee281985a4ec1378
hyfu <71806228>
2015-10-29 15:22:58 +0800

    d

--- src/cg/base/script/GMSVScriptEngine.java
+++
@@ -1,9 +0,0 @@
-package cg.base.script;
-
-import javax.script.Compilable;
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-
-public interface GMSVScriptEngine extends ScriptEngine, Compilable, Invocable {
-
-}
