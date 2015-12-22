c6c3d1f3d1687dee7a915f747b8d15319fab6e8f
hyfu <71806228>
2015-12-22 18:56:14 +0800

    m

--- src/cg/base/map/MapCellContainer.java
+++ src/cg/base/map/MapCellContainer.java
@@ -2,14 +2,6 @@ package cg.base.map;
 
 public interface MapCellContainer {
 	
-	byte CELL_WIDTH = 64;
-	
-	byte CELL_HEIGHT = 48;
-	
-	byte CELL_HALF_WIDTH = CELL_WIDTH >> 1;
-	
-	byte CELL_HALF_HEIGHT = CELL_HEIGHT >> 1;
-	
 	MapCell getMapCell(int east, int south);
 
 }
