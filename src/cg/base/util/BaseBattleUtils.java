335e05ae51f4efd8f89ca726826583634a74dd63
hyfu <71806228>
2015-12-28 16:34:48 +0800

    m

--- src/cg/base/util/BaseBattleUtils.java
+++ src/cg/base/util/BaseBattleUtils.java
@@ -5,17 +5,19 @@ import static cg.base.item.ItemInfo.TYPE_NOT_WEAPON;
 import static cg.base.item.ItemInfo.TYPE_SPEAR;
 import static cg.base.item.ItemInfo.TYPE_STAFF;
 import static cg.base.item.ItemInfo.TYPE_SWORD;
+import static cg.base.item.ItemInfo.TYPE_BOOMERANG;
 import static cg.base.skill.SkillLevelInfo.TECH_SpiracleShot;
-import static cg.base.skill.SkillLevelInfo.TECH_Assassin;;
+import static cg.base.skill.SkillLevelInfo.TECH_Assassin;
+import static cg.base.skill.SkillTemplate.TYPE_ATTACK;
 
-public final class BaseItemUtils {
+public final class BaseBattleUtils {
 	
 	private static final String[] NOT_CLOSE_TECHS = new String[]{
 			TECH_SpiracleShot,
 			TECH_Assassin,
 	};
 	
-	private BaseItemUtils() {}
+	private BaseBattleUtils() {}
 	
 	public static boolean isCloseWeapon(int weaponType) {
 		switch (weaponType) {
@@ -30,8 +32,8 @@ public final class BaseItemUtils {
 		}
 	}
 	
-	public static boolean isCloseCost(int weaponType, String tech) {
-		return !isNotCloseTech(tech) && isCloseWeapon(weaponType);
+	public static boolean isCloseCost(int weaponType, String tech, byte skillType) {
+		return !isNotCloseTech(tech) && skillType == TYPE_ATTACK && isCloseWeapon(weaponType);
 	}
 	
 	private static boolean isNotCloseTech(String tech) {
@@ -41,6 +43,14 @@ public final class BaseItemUtils {
 			}
 		}
 		return false;
+	}
+	
+	public static boolean isLinkedWeapon(int weaponType) {
+		return weaponType == TYPE_BOOMERANG;
+	}
+	
+	public static boolean isLinkedCost(int weaponType, byte skillType) {
+		return skillType == TYPE_ATTACK && isCloseWeapon(weaponType);
 	}
 
 }
